444a0f25ce35207eeebea2f9042364e44e65c4b9
Asem <asem.om95@gmail.com>
2025-06-05 23:24:04 +0300

    final version

--- resources/views/tasks/inedx.blade.php
+++ resources/views/tasks/inedx.blade.php
@@ -15,23 +15,31 @@
   </button>
 
 
-    <x-ui.modal :header="'Add New Task'" :id="'filterModal'">
 
-    <x-ui.form :action_route="route('tasks.index')" :method="'GET'"> 
-                <x-ui.select :name="'status_filter'" :id="'status_filter'" :label="'Status'" :options="$statuses"  :isrequired="false" :value="request('status_filter')"/>  
-                <x-ui.select :name="'priority_filter'" :id="'priority_filter'" :label="'Priority'" :options="$option"  :isrequired="false" :value="request('priority_filter')"/>  
-                <x-ui.input :name="'name_filter'" :id="'name_filter'" :label="'Task name'" :type="'text'" :isrequired="false"  :value="request('name_filter')"/>  
-                <x-ui.input :name="'per_page'" :id="'per_page'" :label="'Items Per Page'"  :type="'number'" :isrequired="false" :value=" request('per_page')"/>  
-    <button type="submit" class="btn btn-primary btn-block mb-4 w-100">Filter</button>
-      </x-ui.form>
-  </x-ui.modal>
 
-  <x-ui.modal :header="'Add New Task'" :id="'newTaskModal'">
+
+<x-ui.table :rows="$tasks">
+</x-ui.table>
+<div class="d-flex justify-content-between align-items-center my-3">
+    <div>
+        Showing page {{ $tasks->currentPage() }} of {{ $tasks->lastPage() }}
+    </div>
+    <div>
+        {{ $tasks->appends(request()->query())->links() }}
+    </div>
+</div>
+
+
+
+
+
+
+    <x-ui.modal :header="'Add New Task'" :id="'newTaskModal'">
     <x-ui.form :action_route="route('task.store')"> 
           <!-- Name input -->
             <x-ui.input :name="'name'" :id="'name'" :label="'Task'" :type="'text'"  :isrequired="true" />  
             <x-ui.input :name="'date'" :id="'date'" :label="'Date'" :type="'date'" :isrequired="true"/>  
-            <x-ui.select :name="'priority'" :id="'priority'" :label="'Priority'" :options="$option"  :isrequired="true", :value="''"/>  
+            <x-ui.select :name="'priority'" :id="'priority'" :label="'Priority'" :options="$option"  :isrequired="true" :value="''"/>  
             <x-ui.text :name="'comments'" :id="'comments'" :label="'Comments'"  :isrequired="true"/>  
           
             
@@ -52,15 +60,16 @@
     </x-ui.form>
   </x-ui.modal>
 
-<x-ui.table :rows="$tasks">
-</x-ui.table>
-<div class="d-flex justify-content-between align-items-center my-3">
-    <div>
-        Showing page {{ $tasks->currentPage() }} of {{ $tasks->lastPage() }}
-    </div>
-    <div>
-        {{ $tasks->appends(request()->query())->links() }}
-    </div>
-</div>
 
+  
+    <x-ui.modal :header="'Filters'" :id="'filterModal'">
+
+    <x-ui.form :action_route="route('tasks.index')" :method="'GET'"> 
+                <x-ui.select :name="'status_filter'" :id="'status_filter'" :label="'Status'" :options="$statuses"  :isrequired="false" :value="request('status_filter')"/>  
+                <x-ui.select :name="'priority_filter'" :id="'priority_filter'" :label="'Priority'" :options="$option"  :isrequired="false" :value="request('priority_filter')"/>  
+                <x-ui.input :name="'name_filter'" :id="'name_filter'" :label="'Task name'" :type="'text'" :isrequired="false"  :value="request('name_filter')"/>  
+                <x-ui.input :name="'per_page'" :id="'per_page'" :label="'Items Per Page'"  :type="'number'" :isrequired="false" :value=" request('per_page')"/>  
+    <button type="submit" class="btn btn-primary btn-block mb-4 w-100">Filter</button>
+      </x-ui.form>
+  </x-ui.modal>
 @endsection
